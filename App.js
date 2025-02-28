80d5ff032bc8e94a3c61e3add11deff1f4ce3301
Mona Padiyar <mona179padiyar@gmail.com>
2025-02-28 21:29:00 +0530

    Ep1 Code

---
+++ App.js
@@ -0,0 +1,8 @@
+const parent = React.createElement("div", {id:"parent"},
+    React.createElement("div", {id:"child"},
+        [React.createElement("h1", {}, "Hi I am h1"),React.createElement("h2", {}, "Hi I am h2")]));
+
+const heading = React.createElement("h1", { id: "heading" }, "hello world from react");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
