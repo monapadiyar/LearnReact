71c384a405e9641c3bc5e588f269dd69be607e50
Mona Padiyar <mona179padiyar@gmail.com>
2025-03-04 18:22:18 +0530

    Ep2 Code

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement("div", {id:"parent"},
     React.createElement("div", {id:"child"},
         [React.createElement("h1", {}, "Hi I am h1"),React.createElement("h2", {}, "Hi I am h2")]));
